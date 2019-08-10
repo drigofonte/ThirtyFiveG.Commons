6c6ab9cb898cae37557914743d5fbb1036d6b609
Rodrigo F. Carvalho <rodrigo@35geeks.com>
2019-08-10 11:08:22 +0100

    Created 'ExtendedEquals' extension method for the 'Object' class to compare numeric values of different types.

---
+++ ThirtyFiveG.Commons.Test/Extensions/ObjectExtensions/ExtendedEqualsTest.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThirtyFiveG.Commons.Extensions;
+
+namespace ThirtyFiveG.Commons.Test.Extensions.ObjectExtensions
+{
+    [TestClass]
+    public class ExtendedEqualsTest
+    {
+        [TestMethod]
+        public void Non_number_and_non_number_equals()
+        {
+            char x = '1';
+            char y = '1';
+
+            Assert.IsTrue(x.ExtendedEquals(y));
+            Assert.IsTrue(y.ExtendedEquals(x));
+        }
+
+        [TestMethod]
+        public void Non_number_and_non_number_not_equals()
+        {
+            char x = '1';
+            char y = '2';
+
+            Assert.IsFalse(x.ExtendedEquals(y));
+            Assert.IsFalse(y.ExtendedEquals(x));
+        }
+
+        [TestMethod]
+        public void Number_and_non_number_not_equals()
+        {
+            int i = 1;
+            char x = '1';
+
+            Assert.IsFalse(x.ExtendedEquals(i));
+            Assert.IsFalse(i.ExtendedEquals(x));
+        }
+
+        [TestMethod]
+        public void Number_and_number_equals()
+        {
+            int i = 1;
+            long l = 1;
+
+            Assert.IsTrue(l.ExtendedEquals(i));
+            Assert.IsTrue(i.ExtendedEquals(l));
+        }
+
+        [TestMethod]
+        public void Number_and_number_not_equals()
+        {
+            int i = 1;
+            long l = 2;
+
+            Assert.IsFalse(l.ExtendedEquals(i));
+            Assert.IsFalse(i.ExtendedEquals(l));
+        }
+    }
+}
--- ThirtyFiveG.Commons/Extensions/ObjectExtensions.cs
+++ ThirtyFiveG.Commons/Extensions/ObjectExtensions.cs
@@ -20,5 +20,28 @@ namespace ThirtyFiveG.Commons.Extensions
 
             return Convert.ChangeType(value, t, null);
         }
+
+        public static bool IsNumber(this object value)
+        {
+            return value is sbyte
+                || value is byte
+                || value is short
+                || value is ushort
+                || value is int
+                || value is uint
+                || value is long
+                || value is ulong
+                || value is float
+                || value is double
+                || value is decimal;
+        }
+
+        public static bool ExtendedEquals(this object n1, object n2)
+        {
+            bool equals = n1 != null && n2 != null;
+            if (equals)
+                equals &= IsNumber(n1) && IsNumber(n2) ? Convert.ToDecimal(n1).Equals(Convert.ToDecimal(n2)) : n1.Equals(n2);
+            return equals;
+        }
     }
 }
